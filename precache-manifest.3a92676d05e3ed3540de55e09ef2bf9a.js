611fb57c8e0dceba3b8b3afeb2c136176073d4cf
Edgardo Rios <edgardo.rios@protonmail.com>
2020-09-30 18:59:06 -0500

    Updates

---
+++ precache-manifest.3a92676d05e3ed3540de55e09ef2bf9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a74c0d7c19bb9cc1fffd4eb4d87427",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "4b6e6730ef7c05d5add0",
+    "url": "/random-quote-machine/static/css/main.22333dcd.chunk.css"
+  },
+  {
+    "revision": "64d2838f17c67b809f38",
+    "url": "/random-quote-machine/static/js/2.7088590a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.7088590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6e6730ef7c05d5add0",
+    "url": "/random-quote-machine/static/js/main.72c53551.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
